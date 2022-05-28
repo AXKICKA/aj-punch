a55bd0089e4aa6416cdd559d5b0abaa12ce20769
Akil Johnson <dermaz@yahoo.com>
2022-05-28 08:04:34 -0400

    Akil - add docs
     Tutorial docs for AWS ECR, Docker, CircleCi workflow

---
+++ terrform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+	backend "s3" {
+		bucket = "axk-tfstate"
+		key = "tf-circle-ecr-demo"
+		region = "us-east-1"
+	}
+	required_providers {
+		circleci = {
+			source = "mrolla/circleci"
+			version = "0.6.1"
+			
+		}
+	}
+}
+
+provider "aws" {
+	profile = "default"
+	region = var.region
+  
+}
+
+provider "circleci" {
+	api_token = var.circleci_cli_token
+	organization = var.organization
+  
+}
