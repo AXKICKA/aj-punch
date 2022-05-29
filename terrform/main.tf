1c3957d65203478fafd7c09b012ffdb5ee3b382d
Akil Johnson <dermaz@yahoo.com>
2022-05-29 05:11:24 -0400

    Akil - Modify ecr demo

--- terrform/main.tf
+++ terrform/main.tf
@@ -1,26 +1,26 @@
 terraform {
-	backend "s3" {
-		bucket = "axk-tfstate"
-		key = "tf-circle-ecr-demo"
-		region = "us-east-1"
-	}
-	required_providers {
-		circleci = {
-			source = "mrolla/circleci"
-			version = "0.6.1"
-			
-		}
-	}
+  backend "s3" {
+    bucket = "axkecr-tfstate"
+    key    = "tf-circle-ecr-demo"
+    region = "us-east-1"
+  }
+  required_providers {
+    circleci = {
+      source  = "mrolla/circleci"
+      version = "0.6.1"
+
+    }
+  }
 }
 
 provider "aws" {
-	profile = "default"
-	region = var.region
-  
+  profile = "default"
+  region  = var.region
+
 }
 
 provider "circleci" {
-	api_token = var.circleci_cli_token
-	organization = var.organization
-  
+  api_token    = var.circleci_cli_token
+  organization = var.organization
+
 }
--- terrform/variables.tf
+++ terrform/variables.tf
@@ -1,44 +1,44 @@
 variable "region" {
-    description = "AWS region to use"
-    default = "us-east-1"
-    type = string
-  
+  description = "AWS region to use"
+  default     = "us-east-1"
+  type        = string
+
 }
 
 variable "environment" {
-    type = string
-    default = "test"
-  
+  type    = string
+  default = "test"
+
 }
 
 variable "name" {
-    description = "Name to use for resources"
-    type = string
-    default = "wordpress"
-  
+  description = "Name to use for resources"
+  type        = string
+  default     = "wordpress"
+
 }
 
 variable "organization" {
-    description = "Github organization to use"
-    type = string
-    default = "AXKICKA"
-  
+  description = "Github organization to use"
+  type        = string
+  default     = "AXKICKA"
+
 }
 
 variable "circleci_cli_token" {
-    description = "CircleCi CLI token to use"
-    type = string
- #   default = "null"  #Nothing set and terraform will ask for it during the apply. Can be set as a terraform var and pass it to terrafrom as you run terraform
+  description = "CircleCi CLI token to use"
+  type        = string
+  #   default = "null"  #Nothing set and terraform will ask for it during the apply. Can be set as a terraform var and pass it to terrafrom as you run terraform
 }
 
 variable "tags" {
-    description = "tags"
-    type = map(string)
+  description = "tags"
+  type        = map(string)
 
-    default = {
-      "managed_by" = "terraform"
-    }
-  
+  default = {
+    "managed_by" = "terraform"
+  }
+
 }
 
 
